b08bf52b60a31e34897d27fb8ae0714556ff6d21
yinwu173 <yin.wu173@gmail.com>
2024-10-03 20:12:22 -0700

    added user model to User.js then exported User

---
+++ models/Blog.js
@@ -0,0 +1,23 @@
+// import {model,datatypes} = require sequelize
+// import sequelize = require ('../config/connection')
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5]
+            }
+        },
+        
+    }
+)
