2d4d3e1c9ebdb2faf4c3553e64af4f6ab5a5d0da
yinwu173 <yin.wu173@gmail.com>
2024-10-03 20:29:02 -0700

    edited blog.js: added code for blog extends model then exported Blog

--- models/Blog.js
+++ models/Blog.js
@@ -1,5 +1,6 @@
-// import {model,datatypes} = require sequelize
-// import sequelize = require ('../config/connection')
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
 
 class Blog extends Model {}
 
@@ -10,14 +11,53 @@ Blog.init(
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
+        }, 
+        destination: {
+            type: DatatTypes.STRING,
+            allowNull: false,
         },
-        username: {
-            type: DataTypes.STRING,
+        trip_rating: {
+            type: DataTypes.INTEGER,
             allowNull: false,
             validate: {
-                len: [5]
+                min: 1,
+                max: 5,
             }
         },
-        
+        budget: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 1
+            }
+        },
+        lodging: {
+            type: DatatTypes.STRING,
+            allowNull: false,
+        },
+        activities: {
+            type: DatatTypes.STRING,
+            allowNull: false,
+        },
+        experience: {
+            type: DatatTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog'
     }
-)
+);
+
+module.exports = Blog;
