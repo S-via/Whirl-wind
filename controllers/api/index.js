56fc008e727924754097b3e9d7f5b22a42ba2f91
S-via <168160925+S-via@users.noreply.github.com>
2024-10-05 15:32:03 -0700

    Revert "Feature/api/blog routes.js"

--- controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-// require path for router and export blog and user routes
-const router = require('express').Router();
-const blogRouter= require('./blogRoutes');
-const userRoutes = require('./userRoutes');
-
-router.use('/blogs',blogRouter);
-router.use('/users',userRoutes);
-
-module.exports = router;
