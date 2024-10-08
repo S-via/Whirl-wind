6ab86a1669d995c5b5699ef5d1a1c4f7052aab5f
Fin M <qbres333@hotmail.com>
2024-10-07 22:18:23 -0600

    add code to signup.js

---
+++ public/css/js/signup.js
@@ -0,0 +1,30 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  // get values from the signup inputs
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  // if both fields have values, send POST request to API endpoint (save user data)
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-type': 'application/json' },
+    });
+
+    if (response.ok) {
+      // if request is successful, redirect to the dashboard
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  } else {
+    alert('Please enter information in all fields');
+  }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
