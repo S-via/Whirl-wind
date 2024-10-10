7c8dd72eec302b2267404a914cc9f8f7be7104ac
S-via <168160925+S-via@users.noreply.github.com>
2024-10-09 20:23:17 -0700

    added GET route, edit Route, put Route, delete Route

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,8 +1,107 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Blog, User, Comment } = require('../../models');
 // inside untils auth.js needs a path to activate this variable
 const withAuth = require('../../utils/auth');
 
+// GET api/blogs
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            attributes: [
+                'id',
+                'destination',
+                'trip_rating',
+                'budget',
+                'lodgin',
+                'activities',
+                'experience'
+            ],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['comments', 'blog_id', 'user_id'],
+
+                }
+            ]
+        });
+        res.status(200).json(blogData)
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+// GET /api/blogs/:id
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'destination',
+                'trip_rating',
+                'budget',
+                'lodging',
+                'activities',
+                'experience'
+            ],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['comments', 'blog_id', 'user_id'],
+
+                },
+            ]
+        })
+        if (!blogData) {
+            res.status(404).json({ message: 'no blog found' })
+            return;
+        }
+        res.json(blogData);
+        
+        
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+// allows to edit post 
+router.get('edit/:id',withAuth,async (req,res)=>{
+    try{
+        const blogData = await Blog.findOne({
+            ...req.body,
+            user_id:req.session.user_id,
+            
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['comments', 'blog_id', 'user_id'],
+                    
+                },
+            ]
+        })
+        if (!blogData) {
+            res.status(404).json({ message: 'no blog found' })
+            return;
+        }
+        res.json(blogData);
+    } catch (err){
+        res.status(500).json(err)
+    }
+});
 
 // user can create new blog post only if logged in
 router.post('/', withAuth, async (req, res) => {
@@ -18,5 +117,55 @@ router.post('/', withAuth, async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+//PUT api/blogs/:id
+
+router.put('/blog/:id', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.update(
+            {
+                ...req.body,
+                user_id: req.session.user_id,
+            },
+            // targets blog post by id 
+            {
+                where: {
+                    id: req.params.id,
+                },
+            })
+        if (!blogData) {
+            res.status(404).json({ message: 'no blog found' });
+            return;
+        }
+        res.json(blogData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// DELETE api/blogs/:id
+router.delete('/blog/:id', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.destroy(
+            {
+                ...req.body,
+                user_id: req.session.user_id,
+            },
+            // targets blog post by id 
+            {
+                where: {
+                    id: req.params.id,
+                },
+            })
+        if (!blogData) {
+            res.status(404).json({ message: 'no blog found' });
+            return;
+        }
+        res.json(blogData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 
 module.exports = router;
